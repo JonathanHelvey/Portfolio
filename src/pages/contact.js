f1f0b342f76e227ac0d7a81483356a8204d3a1d1
JonathanHelvey <jonathanhelvey27@gmail.com>
2019-10-30 15:41:33 -0500

    remove resume

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,7 +3,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SocialMedia from "../components/socialMedia"
 import SEO from "../components/seo"
-import { Link } from "gatsby"
+//import { Link } from "gatsby"
 
 const Contact = ({ data }) => (
   <Layout>
@@ -22,11 +22,11 @@ const Contact = ({ data }) => (
         <div>
           <SocialMedia />
         </div>
-        <div className="my-buttons-container">
+        {/* <div className="my-buttons-container">
           <Link className="resume-button" to="/resume/">
             <button className="resume-button">Resume!</button>
           </Link>
-        </div>
+        </div> */}
         <p>
           <input
             className="form-input"
--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,36 +1,36 @@
 /* eslint-disable react/no-array-index-key */
-import React from "react"
-import resume from "../templates/HelveyResume.pdf"
-import Header from "../components/header"
-import SEO from "../components/seo"
-import Layout from "../components/layout"
+// import React from "react"
+// import resume from "../templates/HelveyResume.pdf"
+// import Header from "../components/header"
+// import SEO from "../components/seo"
+// import Layout from "../components/layout"
 
-const Resume = () => {
-  return (
-    <Layout>
-      <Header />
-      <SEO title="Home" />
-      <div className="resume wrapper2">
-        <p>
-          Looking to get in touch. Send me an email through contact form. Thank
-          you!
-        </p>
-        <div
-          style={{ display: "flex", justifyContent: "center", margin: "0px" }}
-        >
-          <button className="resume-button">
-            <a href={require("../templates/HelveyResume.pdf")} download>
-              Download Resume PDF
-            </a>
-          </button>
-        </div>
-        <object type="application/pdf" data={resume} title="resume">
-          This browser does not support PDFs.
-          <iframe title="Jonathan-Helvey-Resume" />
-        </object>
-      </div>
-    </Layout>
-  )
-}
+// const Resume = () => {
+//   return (
+//     <Layout>
+//       <Header />
+//       <SEO title="Home" />
+//       <div className="resume wrapper2">
+//         <p>
+//           Looking to get in touch. Send me an email through contact form. Thank
+//           you!
+//         </p>
+//         <div
+//           style={{ display: "flex", justifyContent: "center", margin: "0px" }}
+//         >
+//           <button className="resume-button">
+//             <a href={require("../templates/HelveyResume.pdf")} download>
+//               Download Resume PDF
+//             </a>
+//           </button>
+//         </div>
+//         <object type="application/pdf" data={resume} title="resume">
+//           This browser does not support PDFs.
+//           <iframe title="Jonathan-Helvey-Resume" />
+//         </object>
+//       </div>
+//     </Layout>
+//   )
+// }
 
-export default Resume
+// export default Resume
