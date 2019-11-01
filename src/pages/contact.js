ccd97266de1a9d9bcf6fd5517261d95c9fd211d6
JonathanHelvey <jonathanhelvey27@gmail.com>
2019-10-31 21:31:41 -0500

    link

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,7 +3,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SocialMedia from "../components/socialMedia"
 import SEO from "../components/seo"
-//import { Link } from "gatsby"
+import { Link } from "gatsby"
 
 const Contact = ({ data }) => (
   <Layout>
