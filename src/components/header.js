c6d66a57e2b0535e79bb486f6631ce06cd5c9146
JonathanHelvey <jonathanhelvey27@gmail.com>
2019-11-28 14:13:41 -0600

    removed resume

--- src/components/header.js
+++ src/components/header.js
@@ -1,103 +1,103 @@
-/* eslint-disable react/no-array-index-key */
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React, { Component } from "react"
+import { Link } from 'gatsby';
+import React, { Component } from 'react';
 
 class Header extends Component {
   constructor() {
-    super()
-    this.state = { showMenu: false }
-    this.showMenu = this.showMenu.bind(this)
-    this.closeMenu = this.closeMenu.bind(this)
+    super();
+    this.state = { showMenu: false };
+    this.showMenu = this.showMenu.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
   }
+
   showMenu() {
     this.setState({ showMenu: true }, () => {
-      document.addEventListener("click", this.closeMenu)
-    })
+      document.addEventListener('click', this.closeMenu);
+    });
   }
+
   closeMenu() {
     this.setState({ showMenu: false }, () => {
-      document.removeEventListener("click", this.closeMenu)
-    })
+      document.removeEventListener('click', this.closeMenu);
+    });
   }
+
   render() {
+    const { showMenu } = this.state;
     return (
       <header>
         <div className="navbar">
           <ul className="ul-list">
             <li className="links">
               <Link className="links" to="/">
-                <button className="button-links-nav">HOME</button>
+                <button type="button" className="button-links-nav">HOME</button>
               </Link>
             </li>
             <li className="links">
               <Link className="links" to="/#projects">
-                <button className="button-links-nav"> PROJECTS</button>
+                <button type="button" className="button-links-nav"> PROJECTS</button>
               </Link>
             </li>
             <li className="links">
               <Link className="links" to="/#tech">
-                <button className="button-links-nav">ABOUT</button>
+                <button type="button" className="button-links-nav">ABOUT</button>
               </Link>
             </li>
+            {/* <li className="links">
+              <Link className="links" to="/writings/">
+                <button type="button" className="button-links">WRITINGS</button>
+              </Link>
+            </li> */}
             <li className="links">
               <Link className="links" to="/contact/">
-                <button className="button-links-nav">CONTACT</button>
+                <button type="button" className="button-links-nav">CONTACT</button>
               </Link>
             </li>
           </ul>
           <div className="nav-flex-burger">
-            <button className="icon" onClick={this.showMenu}>
+            <button type="button" className="icon" onClick={this.showMenu}>
               <i className="fa fa-bars" />
             </button>
             <div>
-              {this.state.showMenu ? (
+              {showMenu ? (
                 <div className="menu">
                   <div>
                     <Link to="/">
-                      <button className="button-links">Home</button>
+                      <button type="button" className="button-links">Home</button>
                     </Link>
                   </div>
                   <div>
                     <Link to="/#projects">
-                      <button className="button-links">Projects</button>
+                      <button type="button" className="button-links">Projects</button>
                     </Link>
                   </div>
                   <div>
                     <Link to="/#tech">
-                      <button className="button-links">About</button>
+                      <button type="button" className="button-links">About</button>
                     </Link>
                   </div>
                   {/* <div>
                     <Link to="/writings/">
-                      <button className="button-links">Blog</button>
+                      <button type="button" className="button-links">Writings</button>
                     </Link>
                   </div> */}
                   <div>
                     <Link to="/contact/">
-                      <button className="button-links">Contact</button>
+                      <button type="button" className="button-links">Contact</button>
                     </Link>
                   </div>
-                  <div>
+                  {/* <div>
                     <Link to="/resume/">
-                      <button className="button-links">Resume</button>
+                      <button type="button" className="button-links">Resume</button>
                     </Link>
-                  </div>
+                  </div> */}
                 </div>
               ) : null}
             </div>
           </div>
         </div>
       </header>
-    )
+    );
   }
 }
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
+export default Header;
