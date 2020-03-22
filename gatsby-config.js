c99ae6cc8e60776fbf27efb20f955e1db03c740c
JonathanHelvey <jonathanhelvey27@gmail.com>
2020-03-22 15:54:17 -0400

    added sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-transition-link`,
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-filesystem`,
