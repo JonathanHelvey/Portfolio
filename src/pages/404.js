2d36bba7486386bcf176c43741261597e42e3de8
JonathanHelvey <jonathanhelvey27@gmail.com>
2020-03-22 22:54:45 -0400

    updated 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,8 +5,20 @@ import SEO from '../components/seo';
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div style={{ height: '100vh' }}>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop: '12vh', fontSize: '24px' }}>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <h2>404: NOT FOUND</h2>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <img src={require('../images/sad.gif')} alt="sad" />
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: '10px' }}>
+        <p>Please Click Another Link</p>
+      </div>
+    </div>
   </Layout>
 );
 
