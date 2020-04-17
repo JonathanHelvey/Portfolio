d6217e097773e5a1af36b121900531bba6eb23be
JonathanHelvey <jonathanhelvey27@gmail.com>
2020-04-16 21:40:48 -0400

    updated blog posts

--- src/templates/post.js
+++ src/templates/post.js
@@ -8,7 +8,7 @@ export default function Template({ data }) {
   return (
     <Layout>
       <div>
-        <h1>{post.frontmatter.title}</h1>
+        <h1 style={{ textAlign: 'center'}}>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
     </Layout>
